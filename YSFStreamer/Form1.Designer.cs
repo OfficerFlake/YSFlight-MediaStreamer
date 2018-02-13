c6be1f368b10abe9c4df6a0f7fae53ce3cc04f41
Dwayne Brumby <Dwayne.Brumby@outlook.com>
2018-02-13 18:37:19 +1100

    Add project files.

---
+++ YSFStreamer/Form1.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace YSFStreamer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.Streamer = new AxWMPLib.AxWindowsMediaPlayer();
+            this.VolumeSlider = new System.Windows.Forms.TrackBar();
+            this.WaitTimer = new System.Windows.Forms.Label();
+            this.ResyncButton = new System.Windows.Forms.Button();
+            this.BufferingLabel = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.Streamer)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.VolumeSlider)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Streamer
+            // 
+            this.Streamer.AccessibleRole = System.Windows.Forms.AccessibleRole.None;
+            resources.ApplyResources(this.Streamer, "Streamer");
+            this.Streamer.Name = "Streamer";
+            this.Streamer.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("Streamer.OcxState")));
+            this.Streamer.PlayStateChange += new AxWMPLib._WMPOCXEvents_PlayStateChangeEventHandler(this.UpdateLabel);
+            this.Streamer.EndOfStream += new AxWMPLib._WMPOCXEvents_EndOfStreamEventHandler(this.Finish);
+            this.Streamer.Buffering += new AxWMPLib._WMPOCXEvents_BufferingEventHandler(this.UpdateBufferState);
+            // 
+            // VolumeSlider
+            // 
+            resources.ApplyResources(this.VolumeSlider, "VolumeSlider");
+            this.VolumeSlider.BackColor = System.Drawing.Color.Black;
+            this.VolumeSlider.Name = "VolumeSlider";
+            this.VolumeSlider.Value = 10;
+            this.VolumeSlider.ValueChanged += new System.EventHandler(this.AdjustVolume);
+            // 
+            // WaitTimer
+            // 
+            resources.ApplyResources(this.WaitTimer, "WaitTimer");
+            this.WaitTimer.BackColor = System.Drawing.Color.Black;
+            this.WaitTimer.ForeColor = System.Drawing.Color.White;
+            this.WaitTimer.Name = "WaitTimer";
+            // 
+            // ResyncButton
+            // 
+            resources.ApplyResources(this.ResyncButton, "ResyncButton");
+            this.ResyncButton.BackColor = System.Drawing.SystemColors.Control;
+            this.ResyncButton.Name = "ResyncButton";
+            this.ResyncButton.UseVisualStyleBackColor = false;
+            this.ResyncButton.Click += new System.EventHandler(this.Resynrchronise);
+            // 
+            // BufferingLabel
+            // 
+            this.BufferingLabel.BackColor = System.Drawing.Color.Transparent;
+            resources.ApplyResources(this.BufferingLabel, "BufferingLabel");
+            this.BufferingLabel.ForeColor = System.Drawing.Color.White;
+            this.BufferingLabel.Name = "BufferingLabel";
+            // 
+            // Form1
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.BufferingLabel);
+            this.Controls.Add(this.ResyncButton);
+            this.Controls.Add(this.WaitTimer);
+            this.Controls.Add(this.VolumeSlider);
+            this.Controls.Add(this.Streamer);
+            this.Cursor = System.Windows.Forms.Cursors.Default;
+            this.Name = "Form1";
+            this.Shown += new System.EventHandler(this.Init);
+            ((System.ComponentModel.ISupportInitialize)(this.Streamer)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.VolumeSlider)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private AxWMPLib.AxWindowsMediaPlayer Streamer;
+        private System.Windows.Forms.TrackBar VolumeSlider;
+        private System.Windows.Forms.Label WaitTimer;
+        private System.Windows.Forms.Button ResyncButton;
+        private System.Windows.Forms.Label BufferingLabel;
+    }
+}
+
---
+++ YSFStreamer/Form1.cs
@@ -0,0 +1,200 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Net.Sockets;
+using System.IO;
+using System.Globalization;
+
+namespace YSFStreamer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            BufferingLabel.Parent = Streamer;
+            BufferingLabel.BackColor = Color.Transparent;
+            BackColor = Color.Black;
+            BufferingLabel.Visible = false;
+        }
+
+        public void AdjustVolume(object sender, EventArgs e)
+        {
+            Streamer.settings.volume = VolumeSlider.Value * 10;
+        }
+
+        public TimeSpan GetVariance()
+        {
+            var client = new TcpClient("64.90.182.55", 13);
+            using (var streamReader = new StreamReader(client.GetStream()))
+            {
+                var response = streamReader.ReadToEnd();
+                var utcDateTimeString = response.Substring(7, 17);
+                utcDateTimeString = utcDateTimeString.Remove(0, 6).Remove(2) + utcDateTimeString.Remove(0, 2).Remove(4) + utcDateTimeString.Remove(2) + utcDateTimeString.Remove(0, 8);
+                bool ShowTime = Boolean.Parse(File.ReadAllLines("./ShowData.txt")[5]);
+                //if (ShowTime) MessageBox.Show(utcDateTimeString.Remove(0, 8));
+                DateTime Actual = DateTime.Parse(utcDateTimeString);
+                if (ShowTime) MessageBox.Show("Actual: " + Actual.ToString());
+                DateTime ThisSystem = DateTime.UtcNow;
+                if (ShowTime) MessageBox.Show("ThisSystem: " + ThisSystem.ToString());
+                TimeSpan Final = (Actual - ThisSystem);
+                if (ShowTime) MessageBox.Show("Variance: " + Final.ToString());
+                return Final;
+            }
+        }
+
+        public void Init(object sender, EventArgs e)
+        {
+            string PreShow = File.ReadAllLines("./ShowData.txt")[0];
+            string Show = File.ReadAllLines("./ShowData.txt")[1];
+            string PostShow = File.ReadAllLines("./ShowData.txt")[2];
+            bool ShowTime = Boolean.Parse(File.ReadAllLines("./ShowData.txt")[5]);
+            TimeSpan TimeVariance = GetVariance();
+            string StartValue = File.ReadAllLines("./ShowData.txt")[3];
+            DateTime StartTime = DateTime.Parse(File.ReadAllLines("./ShowData.txt")[3]) - TimeVariance;
+            DateTime EndTime = DateTime.Parse(File.ReadAllLines("./ShowData.txt")[4]) - TimeVariance;
+            if (ShowTime) MessageBox.Show(DateTime.UtcNow.ToString());
+            if (ShowTime) MessageBox.Show((DateTime.Parse(File.ReadAllLines("./ShowData.txt")[3]) - TimeVariance).ToString());
+            Streamer.URL = PreShow;
+            Application.DoEvents();
+            this.Refresh();
+            Streamer.Refresh();
+            VolumeSlider.Refresh();
+            while (DateTime.UtcNow < StartTime)
+            {
+                try
+                {
+                    WaitTimer.Text = "The Show will begin in: " + Math.Abs((DateTime.UtcNow - StartTime).Days).ToString() + " Days, " +
+                        Math.Abs((DateTime.UtcNow - StartTime).Hours).ToString() + " Hours, " +
+                        Math.Abs((DateTime.UtcNow - StartTime).Minutes).ToString() + " Minutes, " +
+                        Math.Abs((DateTime.UtcNow - StartTime).Seconds).ToString() + " Seconds.";
+                }
+                catch (Exception exception)
+                {
+                    MessageBox.Show(exception.ToString());
+                }
+                Application.DoEvents();
+            }
+            WaitTimer.Text = "";
+            if (DateTime.UtcNow > EndTime)
+            {
+                WaitTimer.Text = "The Stream has ended.";
+                return;
+            }
+            Streamer.URL = Show;
+            Streamer.Ctlcontrols.currentPosition = 0;
+            Streamer.Ctlcontrols.pause();
+            Streamer.settings.volume = 100;
+            if(DateTime.UtcNow > StartTime && DateTime.UtcNow < EndTime) {
+                Streamer.Ctlcontrols.currentPosition = (DateTime.UtcNow - StartTime).TotalSeconds;
+            }
+            while (DateTime.UtcNow >= StartTime && DateTime.UtcNow <= EndTime) {
+                string mins = (DateTime.UtcNow - StartTime).Minutes.ToString();
+                if (mins.Length == 1)
+                {
+                    mins = "0" + mins;
+                }
+                string secs = (DateTime.UtcNow - StartTime).Seconds.ToString();
+                if (secs.Length == 1)
+                {
+                    secs = "0" + secs;
+                }
+                WaitTimer.Text = mins + ":" + secs;
+                Application.DoEvents();
+            }
+            Streamer.URL = PostShow;
+            WaitTimer.Text = "The Stream has ended.";
+            WaitTimer.ForeColor = System.Drawing.Color.Red;
+        }
+
+        private void Finish(object sender, AxWMPLib._WMPOCXEvents_EndOfStreamEvent e)
+        {
+            WaitTimer.Text = "The Stream has ended.";
+            WaitTimer.ForeColor = System.Drawing.Color.Red;
+        }
+
+        private void Resynrchronise(object sender, EventArgs e)
+        {
+            TimeSpan TimeVariance = GetVariance();
+            DateTime StartTime = DateTime.Parse(File.ReadAllLines("./ShowData.txt")[3]) - TimeVariance;
+            DateTime EndTime = DateTime.Parse(File.ReadAllLines("./ShowData.txt")[4]) - TimeVariance;
+            if (DateTime.UtcNow > StartTime && DateTime.UtcNow < EndTime)
+            {
+                Streamer.Ctlcontrols.currentPosition = (DateTime.UtcNow - StartTime).TotalSeconds;
+            }
+        }
+
+        private void UpdateBufferState(object sender, AxWMPLib._WMPOCXEvents_BufferingEvent e)
+        {
+            if (e.start)
+            {
+                BufferingLabel.Visible = true;
+                BufferingLabel.Text = "Buffering...";
+            }
+            else BufferingLabel.Visible = false;
+        }
+
+        private void UpdateLabel(object sender, AxWMPLib._WMPOCXEvents_PlayStateChangeEvent e)
+        {
+            BufferingLabel.Visible = true;
+            TimeSpan TimeVariance = GetVariance();
+            DateTime StartTime = DateTime.Parse(File.ReadAllLines("./ShowData.txt")[3]) - TimeVariance;
+            DateTime EndTime = DateTime.Parse(File.ReadAllLines("./ShowData.txt")[4]) - TimeVariance;
+            switch (e.newState) {
+                case 1:
+                    BufferingLabel.Text = "Stopped.";
+                    break;
+                case 2:
+                    BufferingLabel.Text = "Playing.";
+                    //Streamer.Ctlcontrols.play();
+                    break;
+                case 3:
+                    BufferingLabel.Visible = false;
+                    break;
+                case 4:
+                case 5:
+                    BufferingLabel.Text = "Scanning.";
+                    //Streamer.Ctlcontrols.play();
+                    break;
+                case 6:
+                    BufferingLabel.Text = "Buffering...";
+                    if (DateTime.UtcNow > StartTime && DateTime.UtcNow < EndTime)
+                    {
+                        Streamer.Ctlcontrols.currentPosition = (DateTime.UtcNow - StartTime).TotalSeconds;
+                    }
+                    break;
+                case 7:
+                    BufferingLabel.Text = "Waiting On Server...";
+                    break;
+                case 8:
+                    BufferingLabel.Text = "End Of Stream";
+                    break;
+                case 9:
+                    BufferingLabel.Text = "Connecting...";
+                    break;
+                case 10:
+                    if (DateTime.UtcNow > StartTime && DateTime.UtcNow < EndTime)
+                    {
+                        Streamer.Ctlcontrols.currentPosition = (DateTime.UtcNow - StartTime).TotalSeconds;
+                        BufferingLabel.Text = "Ready.";
+                    }
+                    else
+                    {
+                        BufferingLabel.Text = "The Show Has Ended.";
+                    }
+                    break;
+                case 11:
+                    BufferingLabel.Text = "Reconnecting";
+                    break;
+                default:
+                    BufferingLabel.Text = "State: " + e.newState.ToString();
+                    break;
+            }
+        }
+    }
+}
